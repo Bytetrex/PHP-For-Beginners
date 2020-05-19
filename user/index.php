ee398456c6fe87e08102b3e2883a25f760fea8a4
Bytetrex <56295147+Bytetrex@users.noreply.github.com>
2020-05-19 18:43:37 +0800

    Update index.php

--- user/index.php
+++ user/index.php
@@ -1,5 +1,5 @@
 <?php
-   require("../includes/config.php");
+   require($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php");
    
    if (isset($_GET['username'])) {
        $username = $_GET['username'];
@@ -21,36 +21,16 @@
                 <div class="column is-5">
                     <p class="is-size-5 has-text-weight-bold"><?php echo $Page->username(); ?></p>
                     <div class="box">
-                        <p class="is-size-6 has-text-grey"><?php echo htmlspecialchars($Page->description()); ?></p>
+                        <p class="is-size-6 has-text-grey"><?php echo htmlspecialchars($Page->bio()); ?></p>
                     </div>
-                    <p class="is-size-5 has-text-weight-bold">Statistics</p>
+                    <p class="is-size-5 has-text-weight-bold">About</p>
                     <div class="box">
-                        <p>Joined:<span class="is-pulled-right has-text-grey"><?php $Page->joined(); ?></span>
-                            <p>Lastseen:<span class="is-pulled-right has-text-grey"><?php $Page->lastseen(); ?></span>
+                        <p>Joined:<span class="is-pulled-right has-text-grey"><?php $Page->joinDate(); ?></span>
+                            <p>Lastseen:<span class="is-pulled-right has-text-grey"><?php $Page->lastSeen(); ?></span>
                     </div>
                 </div>
                 <div class="column">
-                    <p class="is-size-5 has-text-weight-bold">Friends</p>
-                    <div class="box">
-                            <p class="is-size-6 has-text-grey"><span
-                                    class="has-text-weight-bold"><?php echo $page['username']; ?></span> doesn't have
-                                any friends!
-                            </p>
-                    </div>
-                    <p class="is-size-5 has-text-weight-bold">Badges</p>
-                    <div class="box">
-                        <p class="is-size-6 has-text-grey"><span
-                                class="has-text-weight-bold"><?php echo $page['username']; ?></span> doesn't have
-                            any badges!
-                        </p>
-                    </div>
-                    <p class="is-size-5 has-text-weight-bold">Inventory</p>
-                    <div class="box">
-                        <p class="is-size-6 has-text-grey"><span
-                                class="has-text-weight-bold"><?php echo $page['username']; ?></span> doesn't have
-                            any items!
-                        </p>
-                    </div>
+
                 </div>
             </div>
         </div>

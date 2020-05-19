087b8c9d74aefcf182b4a88237d8c014a6eb5f93
Bytetrex <56295147+Bytetrex@users.noreply.github.com>
2020-05-19 19:26:43 +0800

    Update index.php

--- users/index.php
+++ users/index.php
@@ -1,28 +1,22 @@
-<?php
-   require("../includes/config.php");
-?>
-<html>
+<?php require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"); ?>
 
-<body>
     <section class="section">
         <div class="container">
-            <div class="columns">
+            <div class="columns is-multiline">
                 <?php
                   $stmt = $mysqli->prepare("SELECT * FROM users");
                   $stmt->execute();
                   $result = $stmt->get_result();
                   
                   while($row=$result->fetch_array()) {
-                      $description = htmlspecialchars($row['description']);
+                      $bio = htmlspecialchars($row['bio']);
                       echo "<div class='column is-6'><div class='box'>
-                      <p class='is-size-5 has-text-weight-bold'><a href='../user/index.php?username=$row[username]'>$row[username]</a></p>
-                      <p class='has-text-grey'>$description</p>
+                      <p class='is-size-5 has-text-weight-bold'><a href='/user/?username=$row[username]'>$row[username]</a></p>
+                      <p class='has-text-grey'>$bio</p>
                       </div></div>";
                   }
                   ?>
             </div>
         </div>
     </section>
-</body>
-
-</html>
+<?php require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"); ?>

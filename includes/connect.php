2b69669c50bd315e5898edd5aa406ffe9743ad2c
Bytetrex <56295147+Bytetrex@users.noreply.github.com>
2020-05-19 19:02:04 +0800

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,18 @@
 <?php
-$mysqli = new mysqli("localhost", "root", NULL, "Sandbox");
+/* Welcome to the database connection file, here you have to enter
+your MySQL database details.
+
+If your username is not 'root' please change it to the corresponding one,
+if you have a password replace 'NULL' with your password inside speachmarks,
+E.G. "MyPassword"
+
+Make sure to create a database called "main" and upload the sql file either through
+PHPMyAdmin, MySQL workbench, etc.
+
+P.S. Please keep 'localhost' as is.
+*/
+
+$mysqli = new mysqli("localhost", "root", NULL, "main");
 if($mysqli->connect_error) {
     echo "Failed to connect: " . $mysqli->connect_error;
 }
